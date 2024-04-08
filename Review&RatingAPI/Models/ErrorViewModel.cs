fb5184c3fa5794fe99bfdb694a0a1f18dae52399
MateoVG-coding <mateovega10@hotmail.com>
2024-04-08 19:24:09 -0400

    Add project files.

---
+++ Review&RatingAPI/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Review_RatingAPI.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
