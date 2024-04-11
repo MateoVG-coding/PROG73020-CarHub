e3201d1229a98c13bccc5718695ee2633e8292a8
jdavidIP <ibapol2003@gmail.com>
2024-04-11 19:58:34 -0400

    Removed innecessary folders

--- Review&RatingAPI/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Review_RatingAPI.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
