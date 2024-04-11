8c6a9c17450764e34bd33c6d8c1c795fdbf8caee
bzhang4974 <bzhang4974@conestogac.on.ca>
2024-04-11 01:36:12 -0400

    Rebased branch,added reviewModel,reviewsController

---
+++ Review&RatingAPI/Models/Review.cs
@@ -0,0 +1,12 @@
+﻿namespace Review_RatingAPI.Models
+{
+    public class Review
+    {
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public int ListingId { get; set; }
+        public string Content { get; set; } = string.Empty; //Ensure non-null by default
+        public double Rating { get; set; }
+        public DateTime ReviewTime { get; set; }
+    }
+}
