e7b8fa3d5ea67c753a85ecfb87a93b5e494391d3
bzhang4974 <bzhang4974@conestogac.on.ca>
2024-04-11 23:34:21 -0400

    Added review model and basic reviewController

---
+++ WebAPI/Controllers/ReviewController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Review_RatingAPI.Models;
+
+namespace WebAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ReviewsController : ControllerBase
+    {
+        private static List<Review> reviews = new List<Review>();
+
+        [HttpGet]
+        public ActionResult<List<Review>> GetAllReviews()
+        {
+            return reviews;
+        }
+
+        [HttpPost]
+        public IActionResult CreateReview([FromBody] Review review)
+        {
+            if (review == null)
+            {
+                return BadRequest("Review cannot be empty");
+            }
+            reviews.Add(review);
+            return Ok();
+        }
+    }
+}
