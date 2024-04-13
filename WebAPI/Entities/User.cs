a9e454af003f3b90288734b031015dce0e113bf7
jdavidIP <ibapol2003@gmail.com>
2024-04-13 13:08:15 -0400

    Changed User class to fit the one in the master

--- WebAPI/Entities/User.cs
+++ WebAPI/Entities/User.cs
@@ -4,8 +4,21 @@ namespace WebAPI.Entities
 {
     public class User : IdentityUser
     {
+        public int UserId { get; set; }
+
         public string? FirstName { get; set; }
 
         public string? LastName { get; set; }
+
+        public string? RefreshToken { get; set; }
+        public DateTime RefreshTokenExpiryTime { get; set; }
+
+
+        // TO DO: Waiting for implementation of Rating and Review classes
+
+        // public Ratings Rating { get; set; }
+
+        // public Reviews Reviews { get; set; }public string? FirstName { get; set; }
+
     }
 }
