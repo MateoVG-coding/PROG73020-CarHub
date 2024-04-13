d1afa57b591e2d2b6d1ccd70882afdd666f2dbea
jdavidIP <ibapol2003@gmail.com>
2024-04-13 12:11:11 -0400

    Added filter class and routes to the controller

--- WebAPI/Models/Listings.cs
+++ WebAPI/Models/Listings.cs
@@ -4,6 +4,8 @@
     {
         public int listingId { get; set; }
 
+        public DateTime PostingDate { get; set; }
+
         public Cars Car { get; set; }
 
         public Users User { get; set; }
