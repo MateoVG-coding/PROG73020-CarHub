6ebd045ed0da4ee56ba899caf847fc236760a61f
MateoVG-coding <mateovega10@hotmail.com>
2024-04-15 19:00:32 -0400

    Add role configuration for testing purposes.

---
+++ WebAPI/Entities/Configuration/RoleConfiguration.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace WebAPI.Entities.Configuration
+{
+    public class RoleConfiguration : IEntityTypeConfiguration<IdentityRole>
+    {
+        public void Configure(EntityTypeBuilder<IdentityRole> builder)
+        {
+            builder.HasData(
+                new IdentityRole() { Name = "ListingManager", NormalizedName = "LISTING_MANAGER" }
+            );
+        }
+    }
+}
